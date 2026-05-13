6c00fcba1e36c94110ef0d3b089e43b31d9a16a8
nagicome03 <nayeligiselle.0206@gmail.com>
2026-05-13 14:20:20 +0200

    style: secondary button styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Navbar from "./components/navbar/Navbar";
 import PrimaryButton from "./components/PrimaryButton/PrimaryButton";
+import SecondaryButton from "./components/SecondaryButton/SecondaryButton";
 
 function App() {
   return (
     <>
       <Navbar />
       <PrimaryButton/>
-
+      <SecondaryButton/>
     </>
   );
 }
---
+++ src/components/SecondaryButton/SecondaryButton.jsx
@@ -0,0 +1,9 @@
+import "./SecondaryButton.scss";
+
+const SecondaryButton = () => {
+  return (
+    <button className="secondary__button">View Catalog</button>
+  )
+};
+
+export default SecondaryButton;
