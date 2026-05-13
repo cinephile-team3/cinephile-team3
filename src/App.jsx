7cadde1ee69dcc58bce1e89ad17a8bb255ddd676
nagicome03 <nayeligiselle.0206@gmail.com>
2026-05-13 16:44:21 +0200

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/navbar/Navbar";
 import PrimaryButton from "./components/PrimaryButton/PrimaryButton";
 import SecondaryButton from "./components/SecondaryButton/SecondaryButton";
+import Tags from "./components/Tags/Tags";
 
 function App() {
   return (
     <>
+      <Tags label="ULTRA HD STREAMING"/>
       <PrimaryButton label="Get Access"/>
       <SecondaryButton label="View Catalog"/>
     </>
