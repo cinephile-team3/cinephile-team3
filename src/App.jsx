e70a1c61ce5ad976fb9321651b31cd03c982c223
nagicome03 <nayeligiselle.0206@gmail.com>
2026-05-13 17:36:22 +0200

    add: structure and images

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,23 @@ import Tags from "./components/Tags/Tags";
 function App() {
   return (
     <>
-      <Tags label="ULTRA HD STREAMING"/>
-      <PrimaryButton label="Get Access"/>
-      <SecondaryButton label="View Catalog"/>
+      <section className="hero__landing">
+        <Tags label="ULTRA HD STREAMING" />
+        <h1 className="title__landing">Discover Pure Cinema.</h1>
+        <p className="p__landing">
+          Precision-engineered streaming for the world's most dedicated
+          cinephiles.
+        </p>
+        <PrimaryButton label="Get Access" />
+        <SecondaryButton label="View Catalog" />
+      </section>
+      <section className="trending__landing">
+        <article className="card__landing">
+          <img src="" alt="" />
+          <Tags label="NOW TRENDING"/>
+        </article>
+
+      </section>
     </>
   );
 }
