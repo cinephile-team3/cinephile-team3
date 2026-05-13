f6e95c82ca3051bdbc505f21e75449025768bff0
Kressala <chdimaio@protonmail.com>
2026-05-13 16:50:25 +0200

    update: contact page set-up for visualization

--- src/config/router.jsx
+++ src/config/router.jsx
@@ -1,14 +1,14 @@
 import { createBrowserRouter } from "react-router";
 import App from "../App";
-import Contact from "../pages/Contact";
-import Pricing from "../pages/Pricing";
+import Contact from "../pages/Contact/Contact";
+import Pricing from "../pages/Pricing/Pricing";
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <App />
     },
-    {
+   {
         path: "/contact",
         element: <Contact />
     },
