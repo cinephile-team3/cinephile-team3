3bdd72bf0cd55d66266f5cae782f879f7b76403f
Damaris Castro <damaris.icastro@gmail.com>
2026-05-14 19:49:10 +0200

    Feature pricing (#5)
    * changes in pricing and router
    
    * changes in pricing
    
    * pricing card updated
    
    * pricing card updated and solved background issue
    
    * changes in button sizing and ribbon
    
    * changes in spacing in contact page
    
    ---------
    
    Co-authored-by: Nayeli Córdova Mendoza <nayeligiselle.0206@gmail.com>

--- src/config/router.jsx
+++ src/config/router.jsx
@@ -1,19 +1,26 @@
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter, Outlet } from "react-router";
 import App from "../App";
 import Contact from "../pages/Contact/Contact";
 import Pricing from "../pages/Pricing/Pricing";
+import Navbar from "../components/navbar/Navbar";
+import Footer from "../components/Footer/Footer";
+
+const Layout = () => (
+  <>
+    <Navbar />
+    <Outlet />
+    <Footer />
+  </>
+)
 
 export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App />
-    },
-   {
-        path: "/contact",
-        element: <Contact />
-    },
-    {
-        path: "/pricing",
-        element: <Pricing />
-    }
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "/", element: <App /> },
+      { path: "/contact", element: <Contact /> },
+      { path: "/pricing", element: <Pricing /> },
+    ]
+  }
 ]);
