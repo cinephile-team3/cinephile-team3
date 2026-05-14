24adcb889f7a993ebc1dab8819034cd91f547c53
Nayeli Córdova Mendoza <nayeligiselle.0206@gmail.com>
2026-05-14 16:50:02 +0200

    fix: body fixed

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import Tags from "./components/Tags/Tags";
 function App() {
   return (
     <>
-<<<<<<< HEAD
       <main className="landing__container">
         <div className="landing__section1">
           <section className="hero__landing">
@@ -52,45 +51,6 @@ function App() {
           </article>
         </section>
       </main>
-=======
-      <body className="landing__container">
-      <section className="hero__landing">
-        <Tags label="ULTRA HD STREAMING" />
-        <h1 className="title__landing">Discover <span className="title--highlight">Pure</span> Cinema.</h1>
-        <p className="p__landing">
-          Precision-engineered streaming for the world's most dedicated
-          cinephiles.
-        </p>
-        <PrimaryButton label="Get Access" />
-        <SecondaryButton label="View Catalog" />
-      </section>
-      <section className="trending__landing">
-        <article className="card__landing">
-          <Tags label="NOW TRENDING"/>
-          <h2 className="card__title">Interstellar Voyage</h2>
-        </article>
-      </section>
-
-      <section className="kw__landing">
-        <div className="kw__landing__container">
-          <h2 className="kw__title">Keep Watching</h2>
-          <button className="kw__landing__button">See All</button>
-        </div>
-        <article className="kw__landing__card">
-          <figure className="kw__card__figure">
-          <img src="src\assets\kw-landing.jpg" alt="Póster de Neo Tokyo 2088" />
-          </figure>
-          <div className="kw__card__info">
-          <h3 className="kw__card__title">Neo Tokyo 2088</h3>
-          <p className="kw__card__p">S1 : E4 "The Signal"</p>
-          <div className="kw__progress__container">
-            <div className="kw__loading__bar"></div>
-          </div>
-          </div>
-        </article>
-      </section>
-      </body>
->>>>>>> c0526de1b02c5443c5730c54945cdb1ed301ae98
     </>
   );
 }
